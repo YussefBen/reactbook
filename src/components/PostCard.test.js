9b85a331c22fe2a5338843c27d6924b3b0ed6fa1
BEN CHOUCHANE Youssef <yussef.ben@email.com>
2026-01-09 11:34:14 +0100

    Correction du compteur

--- src/components/PostCard.test.js
+++ src/components/PostCard.test.js
@@ -11,17 +11,17 @@ test("PostCard: le clic sur J'aime incrémente et Reset réinitialise le compteu
   );
 
   // Vérifie que le compteur de likes initial est à 0
-  expect(screen.getByText(/0/)).toBeInTheDocument();
+  expect(screen.getByText(/Likes: 0/i)).toBeInTheDocument();
   
   // Simule un clic sur le bouton J'aime
   fireEvent.click(screen.getByRole("button", { name: /j'aime/i }));
   
   // Vérifie que le compteur de likes est maintenant à 1
-  expect(screen.getByText(/1/)).toBeInTheDocument();
+  expect(screen.getByText(/Likes: 1/i)).toBeInTheDocument();
 
   // Simule un clic sur le bouton Reset
   fireEvent.click(screen.getByRole("button", { name: /reinitialiser/i }));
   
   // Vérifie que le compteur de likes est réinitialisé à 0
-  expect(screen.getByText(/0/)).toBeInTheDocument();
+  expect(screen.getByText(/Likes: 0/i)).toBeInTheDocument();
 });

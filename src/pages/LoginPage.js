85dc883f14ddf5138b5cb2c4799b955e69ec3c0b
BEN CHOUCHANE Youssef <yussef.ben@email.com>
2026-01-09 14:22:36 +0100

    ajout des champs email et mot de passe sur la page de connexion

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 
@@ -6,16 +6,39 @@ const LoginPage = () => {
   const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const handleLogin = () => {
-    login({ name: "Yussef" }); // Connexion avec un utilisateur Yussef
-    alert("Connexion réussie !");
-    navigate('/'); // Redirection vers la page d'accueil après connexion
+    email.preventDefault();
+    if (email.trim() !== '' && password.trim() !== '') {
+      login({name: "Yussef", email: email});
+      navigate('/');
+    } else {
+      alert('Veuillez entrer des informations valides.');
+    }
   };
 
   return (
-    <div>
+    <div style={{padding: '20px'}}>
       <h2>Page de Connexion</h2>
-      <button onClick={handleLogin}>Se connecter</button>
+      <form onSubmit={handleLogin}>
+        <div style={{marginBottom: '10px'}}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)} required
+        />
+        </div>
+        <input
+          type="password"
+          placeholder="Mot de passe"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </form>
+      <button type="submit">Se connecter</button>
     </div>
   );
 };
